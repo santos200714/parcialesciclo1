67402bf65f0b354d9cd97dfdffd987155035035d
20071407 <pl033503202@unab.edu.sv>
2026-04-10 22:01:13 -0600

    Parcial 02 Fundamentos de programación, ejercicio 1 variable = elefante se eliminan los espacios en blanco a ambos extremos se cuenta la cantidad de E en la palabra

---
+++ parcial2.1.py
@@ -0,0 +1,11 @@
+variable = "  elefante  "
+
+variable = variable.strip()
+
+E=( variable.count("e"))
+print(f" cantidad de e en la frace={E}")
+caracteres= len(variable)
+
+
+print(f"cantidad de caracteres sin espacios={caracteres}")
+print(f"palabra limpia: '{variable}'")
