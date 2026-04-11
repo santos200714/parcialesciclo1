1432c0b92e467526eb3a2c84155e17c23dd8a49d
20071407 <pl033503202@unab.edu.sv>
2026-04-10 22:41:37 -0600

    Parcial 02 Fundamentos de programación, número del ejercicio 11 toma la cadena el nido matinal limpia los espacios en blanco pone la primera letra de cada palabra en mayuscula despues toma el texto lo centra en un espacio de 30 caracteres rellenando los espacios con guione(-)

---
+++ parcial2.11.py
@@ -0,0 +1,11 @@
+cadena= ("el nido matinal")
+#Limpia los espacios en blanco de los extremos y haz que la primera letra de cada palabra pase a ser mayuscula.
+
+primeraletra=cadena.title()
+print(primeraletra) 
+
+espasioseliminar=cadena.strip()
+print(espasioseliminar)
+
+centrado= primeraletra.center(30,"-")
+print(centrado)
