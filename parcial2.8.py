8a69546e619fd19766fd18216d0b8ba7b879407c
20071407 <pl033503202@unab.edu.sv>
2026-04-10 22:27:54 -0600

    Parcial 02 Fundamentos de programación, número del ejercicio 8 define un bloque de texto de 3 lineas  cuenta cuantas veces aparece la letra A en todo el texto divide el bloque de texto por su salto en linea usando(splitlines)

---
+++ parcial2.8.py
@@ -0,0 +1,10 @@
+texto= """"Mientras alguien conserve la esperanza,
+las promesas perdurarán a pesar de la muerte. La 
+esperanza mantiene vivos los sueños"."""
+
+A=( texto.count("a"))
+print(f" cantidad de A en la frace={A}")
+
+
+divide= texto.splitlines()
+print(divide)   
