57f0ea6c7255d951893b14e9faee601e15ab0066
santos200714 <pl0335032026@unab.edu.sv>
2026-05-15 17:05:32 -0600

    Delete parcial2.6.py

--- parcial2.6.py
+++
@@ -1,9 +0,0 @@
-
-texto = "crithofer santos portillo landaverde"
-
-texto_normalizado = texto.casefold()
-
-resultado = texto_normalizado.replace(" ", "").isalpha()
-
-print(texto)
-print(resultado)
